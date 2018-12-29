18b8e28cbcbe4fcc3878204b838d5b156f3a193e
fabrice <fgarrec@hotmail.com>
2018-12-29 15:28:35 -0500

    portfolio widget v2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,38 @@
 
 
 
-    <h1>Section Portfolios</h1>    
+    <h1>Section Portfolios</h1> 
+    
+    <div class="portfolio-wrapper">
+            <div class="students-header col-12">
+                <a href="portfolio.html">
+                    <p class="title-carousel">Section portfolio</p>
+                </a>
+            </div>
+            <div class="row">
+                @foreach ($users as $user)
+                    <div class="portfolio-container students-grid col-12 col-sm-6 col-md-4">
+                        <div class="portfolios-widget">
+                            <div class="portfolios-category">
+                                <h3 class="portfolios-category-title">Multimédia</h3>
+                            </div>
+                            <header class="portfolios-header" style="background-image: url(img/user_couverture/portfolio-bg.jpg); background-size: cover;">
+                                <img src="img/avatars/1534274813.jpg" class="profil-img profil-img-portfolio-widget" alt="">
+                                <p class="portfolios-name">{{$user->firstName}} {{$user->lastName}}</p>
+                            </header>
+                            <div class="portfolios-body">
+                                <p class="portfolios-nb-views">105 vues</p>
+                                <p class="portfolios-resume">
+                                        {{$user->description}} 
+                                        {{-- /*raccourcir la description*/ --}}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    
+                @endforeach
+            </div>
+        </div>
     
     <div class="row"></div>
     
