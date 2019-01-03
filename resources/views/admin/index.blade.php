741a453717034f9960d51cfbf02703a1e593763e
Fredstyle93 <fred.beaulieu123@gmail.com>
2019-01-03 15:08:46 -0500

    Création des onglets du menu

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,8 +2,77 @@
 
 @section('content')
 
-<div class="col-md-4">
+
 
     <p>stats</p>
-</div>
+
+    <div class="col-md-3">
+        <div class="box box-default bg-aqua collapsed-box">
+          <div class="box-header with-border">
+              <div class="row">
+                <div class="col-md-6 info-box">
+
+                    <div class="inner">
+                            <h3>150</h3>
+              
+                            <p>Users</p>
+                          </div>
+                </div>
+                        <div class="col-md-6">
+
+                            <div class="icon">
+                                  <i class="fa fa-shopping-cart fa-5x"></i>
+                                </div>
+                        </div>
+              </div>
+
+            <div class="box-tools pull-right">
+              <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i>
+              </button>
+            </div>
+            <!-- /.box-tools -->
+          </div>
+          <!-- /.box-header -->
+          <div class="box-body">
+            The body of the box
+          </div>
+          <!-- /.box-body -->
+        </div>
+        <!-- /.box -->
+      </div>
+
+      <div class="col-lg-3 col-xs-6">
+        <!-- small box -->
+        <div class="small-box bg-aqua">
+          <div class="inner">
+            <h3>150</h3>
+
+            <p>New Orders</p>
+          </div>
+          <div class="icon">
+            <i class="fa fa-shopping-cart"></i>
+          </div>
+          <a href="#" class="small-box-footer">
+            More info <i class="fa fa-arrow-circle-right"></i>
+          </a>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-xs-6">
+        <!-- small box -->
+        <div class="small-box bg-aqua">
+          <div class="inner">
+            <h3>150</h3>
+
+            <p>New Orders</p>
+          </div>
+          <div class="icon">
+            <i class="fa fa-shopping-cart"></i>
+          </div>
+          <a href="#" class="small-box-footer">
+            More info <i class="fa fa-arrow-circle-right"></i>
+          </a>
+        </div>
+      </div>
+
 @endsection
--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -25,36 +25,11 @@
           <!-- sidebar menu: : style can be found in sidebar.less -->
           <ul class="sidebar-menu" data-widget="tree">
             <li class="header">MAIN NAVIGATION</li>
-            <li class="active treeview">
-              <a href="#">
-                <i class="fa fa-dashboard"></i> <span>Dashboard</span>
-                <span class="pull-right-container">
-                  <i class="fa fa-angle-left pull-right"></i>
-                </span>
-              </a>
-              <ul class="treeview-menu">
-                <li class="active"><a href="index.html"><i class="fa fa-circle-o"></i> Dashboard v1</a></li>
-                <li><a href="index2.html"><i class="fa fa-circle-o"></i> Dashboard v2</a></li>
-              </ul>
-            </li>
-            <li class="treeview">
-              <a href="#">
-                <i class="fa fa-files-o"></i>
-                <span>Layout Options</span>
-                <span class="pull-right-container">
-                  <span class="label label-primary pull-right">4</span>
-                </span>
-              </a>
-              <ul class="treeview-menu">
-                <li><a href="pages/layout/top-nav.html"><i class="fa fa-circle-o"></i> Top Navigation</a></li>
-                <li><a href="pages/layout/boxed.html"><i class="fa fa-circle-o"></i> Boxed</a></li>
-                <li><a href="pages/layout/fixed.html"><i class="fa fa-circle-o"></i> Fixed</a></li>
-                <li><a href="pages/layout/collapsed-sidebar.html"><i class="fa fa-circle-o"></i> Collapsed Sidebar</a></li>
-              </ul>
-            </li>
+
+
             <li>
               <a href="pages/widgets.html">
-                <i class="fa fa-th"></i> <span>Widgets</span>
+                <i class="fa fa-th"></i> <span>Home</span>
                 <span class="pull-right-container">
                   <small class="label pull-right bg-green">new</small>
                 </span>
@@ -62,47 +37,48 @@
             </li>
             <li class="treeview">
               <a href="#">
-                <i class="fa fa-pie-chart"></i>
-                <span>Charts</span>
+                <i class="fa fa-plus"></i>
+                <span>Ajouter</span>
                 <span class="pull-right-container">
                   <i class="fa fa-angle-left pull-right"></i>
                 </span>
               </a>
               <ul class="treeview-menu">
-                <li><a href="pages/charts/chartjs.html"><i class="fa fa-circle-o"></i> ChartJS</a></li>
-                <li><a href="pages/charts/morris.html"><i class="fa fa-circle-o"></i> Morris</a></li>
-                <li><a href="pages/charts/flot.html"><i class="fa fa-circle-o"></i> Flot</a></li>
-                <li><a href="pages/charts/inline.html"><i class="fa fa-circle-o"></i> Inline charts</a></li>
+                <li><a href="pages/charts/chartjs.html"><i class="fa fa-circle-o"></i> Catégorie</a></li>
+                <li><a href="pages/charts/morris.html"><i class="fa fa-circle-o"></i> École</a></li>
+                <li><a href="pages/charts/flot.html"><i class="fa fa-circle-o"></i> Compétences</a></li>
+                <li><a href="pages/charts/inline.html"><i class="fa fa-circle-o"></i> Section</a></li>
               </ul>
             </li>
             <li class="treeview">
               <a href="#">
-                <i class="fa fa-laptop"></i>
-                <span>UI Elements</span>
+                  <i class="fa fa-edit"></i>
+                <span>Modifier</span>
                 <span class="pull-right-container">
                   <i class="fa fa-angle-left pull-right"></i>
                 </span>
               </a>
               <ul class="treeview-menu">
-                <li><a href="pages/UI/general.html"><i class="fa fa-circle-o"></i> General</a></li>
-                <li><a href="pages/UI/icons.html"><i class="fa fa-circle-o"></i> Icons</a></li>
-                <li><a href="pages/UI/buttons.html"><i class="fa fa-circle-o"></i> Buttons</a></li>
-                <li><a href="pages/UI/sliders.html"><i class="fa fa-circle-o"></i> Sliders</a></li>
-                <li><a href="pages/UI/timeline.html"><i class="fa fa-circle-o"></i> Timeline</a></li>
-                <li><a href="pages/UI/modals.html"><i class="fa fa-circle-o"></i> Modals</a></li>
+                <li><a href="pages/UI/general.html"><i class="fa fa-circle-o"></i> Catégorie</a></li>
+                <li><a href="pages/UI/icons.html"><i class="fa fa-circle-o"></i> École</a></li>
+                <li><a href="pages/UI/buttons.html"><i class="fa fa-circle-o"></i> Compétence</a></li>
+                <li><a href="pages/UI/sliders.html"><i class="fa fa-circle-o"></i> Section</a></li>
               </ul>
             </li>
             <li class="treeview">
               <a href="#">
-                <i class="fa fa-edit"></i> <span>Forms</span>
+                  <i class="fa fa-times"></i> <span>Supprimer</span>
                 <span class="pull-right-container">
                   <i class="fa fa-angle-left pull-right"></i>
                 </span>
               </a>
               <ul class="treeview-menu">
-                <li><a href="pages/forms/general.html"><i class="fa fa-circle-o"></i> General Elements</a></li>
-                <li><a href="pages/forms/advanced.html"><i class="fa fa-circle-o"></i> Advanced Elements</a></li>
-                <li><a href="pages/forms/editors.html"><i class="fa fa-circle-o"></i> Editors</a></li>
+                <li><a href="pages/forms/general.html"><i class="fa fa-circle-o"></i> Catégorie</a></li>
+                <li><a href="pages/forms/advanced.html"><i class="fa fa-circle-o"></i> École</a></li>
+                <li><a href="pages/forms/editors.html"><i class="fa fa-circle-o"></i> Compétence</a></li>
+                <li><a href="pages/UI/sliders.html"><i class="fa fa-circle-o"></i> Section</a></li>
+                <li><a href="pages/UI/sliders.html"><i class="fa fa-circle-o"></i> Usager</a></li>
+                <li><a href="pages/UI/sliders.html"><i class="fa fa-circle-o"></i> Portfolio</a></li>
               </ul>
             </li>
             <li class="treeview">
@@ -188,11 +164,7 @@
                 <li><a href="#"><i class="fa fa-circle-o"></i> Level One</a></li>
               </ul>
             </li>
-            <li><a href="https://adminlte.io/docs"><i class="fa fa-book"></i> <span>Documentation</span></a></li>
-            <li class="header">LABELS</li>
-            <li><a href="#"><i class="fa fa-circle-o text-red"></i> <span>Important</span></a></li>
-            <li><a href="#"><i class="fa fa-circle-o text-yellow"></i> <span>Warning</span></a></li>
-            <li><a href="#"><i class="fa fa-circle-o text-aqua"></i> <span>Information</span></a></li>
+
           </ul>
         </section>
         <!-- /.sidebar -->
